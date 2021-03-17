dfec65523cbd8d12427c0fa8828a8000a36ead94
Bruno Virlet <bvirlet@users.noreply.github.com>
2021-03-17 17:12:08 +0100

    Revert static type
    Causing error Termination Description: DYLD, dyld: chained fixups, seg_count exceeds number of segments

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OpenSSL",
-            type: .static,
             targets: ["OpenSSL", "OpenSSL-local"]
         )
     ],
